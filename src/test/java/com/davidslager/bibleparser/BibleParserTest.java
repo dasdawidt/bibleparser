0bc4e490c1ba2828162f74bf7c5ad3e8d7f517f8
dasdawidt <David_Maier@gmx.de>
2023-11-30 14:33:26 +0100

    feat: conversion to json

--- src/test/java/com/davidslager/bibleparser/BibleParserTest.java
+++ src/test/java/com/davidslager/bibleparser/BibleParserTest.java
@@ -2,8 +2,11 @@ package com.davidslager.bibleparser;
 
 import com.davidslager.bible.Translation;
 
+import javax.json.bind.JsonbBuilder;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +23,14 @@ class BibleParserTest {
         }
         assertNotNull(translation);
         System.out.println(translation);
+        try (
+                var writer = new PrintWriter("bible.json");
+                var jsonb = JsonbBuilder.create()
+        ) {
+            writer.write(jsonb.toJson(translation));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
