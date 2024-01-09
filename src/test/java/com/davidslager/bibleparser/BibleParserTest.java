b6cdf2850ede042d157bb5d2eb9afc0ce4d7cf99
dasdawidt <David_Maier@gmx.de>
2024-01-09 22:02:29 +0100

    refactor: clean up imports

--- src/test/java/com/davidslager/bibleparser/BibleParserTest.java
+++ src/test/java/com/davidslager/bibleparser/BibleParserTest.java
@@ -4,7 +4,6 @@ import com.davidslager.bible.Translation;
 
 import javax.json.bind.JsonbBuilder;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
